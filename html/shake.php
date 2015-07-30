98e3a166e52d6c1d2de68705cbe90c58aefb452f
xiayuanchuan <xiayuanchuan@bravesoft.co.jp>
2015-07-31 00:55:00 +0800

    摇一摇  页面  休眠两秒

--- html/shake.php
+++ html/shake.php
@@ -12,22 +12,28 @@ and open the template in the editor.
         <script src="./js/jquery.min.js"></script>
         <script type="text/javascript">
             var color = new Array('#fff', '#ff0', '#f00', '#000', '#00f', '#0ff');
-            var id ="<?php echo isset($_GET['id']) ? $_GET['id'] : '-1'; ?>";
+            var id = "<?php echo isset($_GET['id']) ? $_GET['id'] : '-1'; ?>";
             if (window.DeviceMotionEvent) {
                 var speed = 25;
                 var x = y = z = lastX = lastY = lastZ = 0;
-                window.addEventListener('devicemotion', function() {
+                window.addEventListener('devicemotion', function () {
                     var acceleration = event.accelerationIncludingGravity;
                     x = acceleration.x;
                     y = acceleration.y;
                     if (Math.abs(x - lastX) > speed || Math.abs(y - lastY) > speed) {
-                        //这里面写异步加载处理
-                        $.getJSON("../control/shake.php?id=" + id, function(data) {
+                        //这里面写异步加载处理  减少服务器压力   休眠2 秒钟
+                        var dataobj=new Date();
+                        var start = dataobj.getTime();
+                        while (true) {
+                            if (dataobj.getTime() - start > 2000)
+                                break;
+                        }
+                        $.getJSON("../control/shake.php?id=" + id, function (data) {
                             $("#info").html("");
                             var info = " <table>" +
-                                    "<tr> <td> 开始时间 </td><td>"+data.start+"</td > </tr>" +
-                                    "<tr> <td> 结束时间 </td><td>"+data.end+"</td > </tr>" +
-                                    "<tr> <td> 累计摇一摇次数 </td><td>"+data.total+"</td> </tr>" +
+                                    "<tr> <td> 开始时间 </td><td>" + data.start + "</td > </tr>" +
+                                    "<tr> <td> 结束时间 </td><td>" + data.end + "</td > </tr>" +
+                                    "<tr> <td> 累计摇一摇次数 </td><td>" + data.total + "</td> </tr>" +
                                     "</table>";
                             $("#info").html(info);
 
