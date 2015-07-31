b91186481e6d802f60c4708b555229e9dce8a55f
yushuijin <yushuijin@bravesoft.co.jp>
2015-07-31 17:12:23 +0800

    修改点赞

--- html/pointlike.php
+++ html/pointlike.php
@@ -21,12 +21,22 @@
     <body>
         <script>
             $(function() {
+                function sleep(n) { //n表示的毫秒数
+                    var start = new Date().getTime();
+                    while (true)
+                        if (new Date().getTime() - start > n)
+                            break;
+                }
                 $("#ilike").click(function() {
+                     $("#info").html("");
                     // 异步加载投票
-                    var c_no=$("#c_no").val();
-                    var opid=$("#opid").val();
-                    $.getJSON("../control/ilike.php?c_no="+c_no+"&opid="+opid, function(data) {
-                        $("#info").html("");
+                    var c_no = $("#c_no").val();
+                    var opid = $("#opid").val();
+                    $("#ilike").val('点赞中....');
+                    $.getJSON("../control/ilike.php?c_no=" + c_no + "&opid=" + opid, function(data) {
+                          sleep(1000);
+                          $("#ilike").val('点赞');
+                       
                         if (data.statu < 4) {
                             var str = " <table>" +
                                     "<tr><td>提示：</td><td>" + data.mesg + "</td></tr>" +
@@ -41,12 +51,13 @@
             });
         </script>
         <div>
-            <input id="c_no" type="hidden" value="<?php echo isset($_GET['c_no']) ? $_GET['c_no'] : '';?>" />
-            <input id="opid" type="hidden" value="<?php echo isset($_GET['opid']) ? $_GET['opid'] : '';?>" />
+            <input id="c_no" type="hidden" value="<?php echo isset($_GET['c_no']) ? $_GET['c_no'] : ''; ?>" />
+            <input id="opid" type="hidden" value="<?php echo isset($_GET['opid']) ? $_GET['opid'] : ''; ?>" />
         </div>
         <div>
-            <button id="ilike"  type="submit">点赞</button>
+            <input id="ilike"  type="button" value="点赞"/>
         </div>
+        <div id="m"></div>
         <div id="info">
 
         </div>
